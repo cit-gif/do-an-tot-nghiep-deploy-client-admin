66df9b7fec40025403cb30ef8b97534efab35c05
cit-git <dsba.19i@cit.udn.vn>
2021-11-26 16:27:41 +0700

    dd

--- src/config/api.js
+++ src/config/api.js
@@ -4,7 +4,9 @@ import { serverApi } from './constrant';
 const api = axios.create({
 	baseURL: serverApi,
 	withCredentials: true,
-	'Access-Control-Allow-Origin': '*',
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+	},
 });
 
 export default api;
